584c95c84c2d91219de6e8405b012deb090d07c6
Ken Huang <hunm4dj0@gmail.com>
2019-08-26 22:36:23 +0800

    list comprehension

--- review.py
+++ review.py
@@ -1,10 +1,10 @@
-data = []
-count = 0
-with open('reviews.txt', 'r') as f:
+data = [] 
+count = 0 
+with open('reviews.txt', 'r') as f: 
 	for line in f:
 		data.append(line)
-		count += 1
-		if count % 1000 == 0:
+		count += 1 
+		if count % 1000 == 0: 
 			print(len(data))
 print('檔案讀取完了, 總共有', len(data), '筆資料')
 
@@ -27,5 +27,11 @@ good = []
 for d in data:
 	if 'good' in d:
 		good.append(d)
+# good = [d for d in data if 'good' in d]
 print('一共有', len(good), '筆流言有提到good')
 print(good[0])
+
+# bad = ['bad in d' for d in data]
+bad = []
+for d in data:
+	bad.append('bad' in d) # 存取True and False
